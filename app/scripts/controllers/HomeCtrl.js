af4c04f98326ba401664111aeefd50ed5111eafd
Diego Aguilar <diegoaguilar@Diegos-MacBook-Pro-2.local>
2017-05-15 08:26:28 -0700

    4: List Messages and Toggle

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,13 +1,19 @@
 (function() {
-     function HomeCtrl(Room, $uibModal) {
+     function HomeCtrl(Room, Message, $uibModal) {
        this.rooms = Room.all;
 
-   this.open = function (size) {
-    this.modalInstance = $uibModal.open({
+       this.currentRoom = "";
+
+       this.messages = "";
+
+       this.roomShow = false;
+
+       this.open = function (size) {
+       this.modalInstance = $uibModal.open({
           animation: this.animationsEnabled,
           templateUrl: '/templates/modal.html',
           controller: 'ModalInstanceCtrl',
-        controllerAs: '$ctrl',
+          controllerAs: '$ctrl',
           size: size ? size : 'sm',
           resolve: {
             items: function () {
@@ -17,10 +23,19 @@
         });
       }
 
+      this.setClickedRoom = function(clickedRoom) {
+        this.currentRoom = clickedRoom;
+        this.messages = Message.getByRoomId(this.currentRoom.$id);
+      }
+
+      this.setRoomShow = function() {
+        this.roomShow = true;
+      }
+
 
      }
 
      angular
          .module('blocChat')
-         .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+         .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
  })();
