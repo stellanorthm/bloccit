d34e63b8b193617b0e2a9dba82da597ebb6918f2
Diego Aguilar <diego.a.mazzero@gmail.com>
2017-07-03 07:13:54 -0700

    12Checkpoint-Assignment: Added contact page and route

--- bloccit/config/routes.rb
+++ bloccit/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contact'
+
+  
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
