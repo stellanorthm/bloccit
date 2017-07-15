e35733dd39be414759563c3921d7d772fbf674b4
Diego Aguilar <diego.a.mazzero@gmail.com>
2017-07-15 08:20:04 -0700

    checkpoint-22: added topics to bloccit

--- bloccit/config/routes.rb
+++ bloccit/config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   resources :advertisements
 
-  resources :posts
+  resources :topics do
+     resources :posts, except: [:index]
+   end
 
   get 'about' => 'welcome#about'
 
