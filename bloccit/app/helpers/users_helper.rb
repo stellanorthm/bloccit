ba72ccfbcfbbbfc3b9a65d7d4eeb34ecc7979b6c
Diego Aguilar <diego.a.mazzero@gmail.com>
2017-07-26 08:12:48 -0700

    assignment-32: factories for posts and display features in user show

--- bloccit/app/helpers/users_helper.rb
+++ bloccit/app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def user_has_posts_comments
+    user.posts && user.comments && user.favorites
+  end
 end
---
+++ bloccit/spec/factories/votes.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+   factory :vote do
+     user
+     post
+     value 1
+   end
+ end
